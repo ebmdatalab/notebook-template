54eafc42006462f14cbc8813b709d18b32cda49c
Seb Bacon <seb.bacon@gmail.com>
2020-01-09 11:05:39 +0000

    Run commands in root's home directory

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t custom-docker -f config/Dockerfile .
-docker run -ti -v ${PWD}:/usr/local/bin/custom-docker -p 8888:8888 custom-docker
+docker run -ti -v ${PWD}:/root -p 8888:8888 custom-docker
