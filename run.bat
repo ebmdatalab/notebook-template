ff78fd4c284d0be062a0ea14aff62236aa1a2ee3
Seb Bacon <seb.bacon@gmail.com>
2020-01-13 09:56:43 +0000

    Use our own base docker image
    Crucially, this contains jupytext
    
    But also gives us fine-grained control over our environment

--- run.bat
+++ run.bat
@@ -1,6 +1,4 @@
-#!/bin/bash
-
 SET mypath=%~dp0
 
-docker build -t jupyter-docker -f config/Dockerfile .
-docker run -ti --mount source=%mypath:~0,-1%,dst=/home/app/notebook,type=bind -p 8888:8888 jupyter-docker
+docker build -t datalab-jupyter -f config/Dockerfile .
+docker run --rm -ti --mount source=%mypath:~0,-1%,dst=/home/app/notebook,type=bind -p 8888:8888 datalab-jupyter
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t jupyter-docker -f config/Dockerfile .
-docker run -ti --mount source=${PWD},dst=/home/app/notebook,type=bind -p 8888:8888 jupyter-docker
+docker build -t datalab-jupyter -f config/Dockerfile .
+docker run --rm -ti --mount source=${PWD},dst=/home/app/notebook,type=bind -p 8888:8888 datalab-jupyter
