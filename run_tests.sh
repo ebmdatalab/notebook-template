6f1644913d0c50dc85670a08dd14d919731d6dcf
Seb Bacon <seb.bacon@gmail.com>
2020-01-21 14:37:24 +0000

    Run tests with correct environment, and ignoring meaningless diffs

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# This awkward invocation is to get around the case where no tests are
-# found, which has exit code of 5 in pytest
+# This awkward testing of exit codes is to get around the case where
+# no tests are found, which has exit code of 5 in pytest, but we don't
+# want to treat as a failure
 
-py.test --nbval notebooks; ret=$?; [ $ret = 5 ] && exit 0 || exit $ret
+PYTHONPATH=$(pwd) python -m pytest --sanitize-with config/nbval_sanitize_file.conf --nbval notebooks; ret=$?; [ $ret = 5 ] && exit 0 || exit $ret
