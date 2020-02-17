4df85f5b451b51f1189f9295243fca75c20cf7cd
Seb Bacon <seb.bacon@gmail.com>
2020-02-17 16:38:16 +0000

    Run docker in the background, so we can find out the mapped port
    Closes #15

--- run.bat
+++ run.bat
@@ -1,9 +1,13 @@
 SET mypath=%~dp0
 
 docker build -t datalab-jupyter -f Dockerfile .
-docker run --rm -ti --mount source=%mypath:~0,-1%,dst=/home/app/notebook,type=bind -p 8888:8888 datalab-jupyter
+
+SET pid=
+for /f "delims=" %%a in ('docker run --detach --rm -ti --mount source=%mypath:~0,-1%,dst=/home/app/notebook,type=bind --publish-all datalab-jupyter') do $set PID=%%a
+docker port %pid
 
 if errorlevel 1 (
-   echo Pres ENTER to close this window
+   echo Press ENTER to close this window
+   echo Stop Jupyter using the File -> Shutdown menu option in Jupyterlab
    pause > nul
 )
--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 docker build -t datalab-jupyter -f Dockerfile .
-docker run --rm -ti --mount source=${PWD},dst=/home/app/notebook,type=bind -p 8888:8888 datalab-jupyter
+PID=$(docker run --detach --rm -ti --mount source=${PWD},dst=/home/app/notebook,type=bind --publish-all datalab-jupyter)
+echo "Set up port mapping:"
+docker port $PID
+echo "Stop Jupyter using the File -> Shutdown menu option in Jupyterlab"
