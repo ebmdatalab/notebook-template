c0b74c3a8cfbb3b1dc70a5e2fb89b600a3e98d51
Seb Bacon <seb.bacon@gmail.com>
2020-02-18 22:22:52 +0000

    Remove redundant files

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker build -t datalab-jupyter -f Dockerfile .
-PID=$(docker run --detach --rm -ti --mount source=${PWD},dst=/home/app/notebook,type=bind --publish-all datalab-jupyter)
-echo "Set up port mapping:"
-docker port $PID
-echo "Stop Jupyter using the File -> Shutdown menu option in Jupyterlab"
